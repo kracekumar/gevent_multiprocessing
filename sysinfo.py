3f9d674440b83fe322fd03c12810bf216700b57c
kracekumar <kracethekingmaker@gmail.com>
2012-05-11 20:26:11 +0530

    fixed multiprocessor

--- sysinfo.py
+++ sysinfo.py
@@ -4,13 +4,13 @@
 #This provides info about running machine
 
 import platform
-
+from multiprocessing import cpu_count
 def print_info():
     print("=== Machine Details ===")
     print("Architecture: {0}".format(platform.architecture()))
     print("Dist: {0}".format(platform.dist()))
     print("Processor: {0}".format(platform.processor()))
-    print("Total Cores: {0}".format(total_processors))
+    print("Total Cores: {0}".format(cpu_count()))
 
 if __name__ == "__main__":
     print_info()
