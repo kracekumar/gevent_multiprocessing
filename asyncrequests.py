e69283e88a48b43e5e71df6bded19968436b0147
kracekumar <kracethekingmaker@gmail.com>
2012-05-13 00:11:51 +0530

    added contents to write to disk

--- asyncrequests.py
+++ asyncrequests.py
@@ -6,6 +6,8 @@
 """
 from requests import async
 import datetime 
+from gevent import monkey; monkey.patch_all()
+import gevent
 
 def get_files_to_download():
     files_to_download = []
@@ -14,12 +16,20 @@ def get_files_to_download():
             files_to_download.append(to_download.split('\n')[0])
     return files_to_download
 
+def write_to_disk(response):
+    if response.ok:
+        with open(response.headers['content-disposition'].split('=')[1],
+                          'wb') as f:
+            f.write(dl_file.content)
+    
 def main():
     urls = get_files_to_download()
     start = datetime.datetime.now()
     print("Requests aync started")
     rs = [async.get(url) for url in urls]
     responses = async.map(rs)
+    jobs = [gevent.spawn(write_to_disk, response) for response in responses]
+    gevent.joinall(jobs)
     print("Requests async took {0} seconds for {1} urls".format(\
           datetime.datetime.now() - start, len(urls)))
 
